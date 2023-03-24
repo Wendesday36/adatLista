2176834db336020a7bfec2eeefe1abf3c6dffb80
Wendesday36 <bertazoeanna@gmail.com>
2023-03-24 13:45:21 +0100

    lor

--- main.js
+++ main.js
@@ -1,5 +1,26 @@
-import { ADATLISTA } from "./adat";
-osszeAllit(ADATLISTA)
-function osszeAllit(lista){
-    
+import { ADATLISTA } from "./adat.js";
+$(function () {
+  const articleElem = $("article");
+  articleElem.append(osszeAllit(ADATLISTA));
+
+  // jQuery methods go here...
+});
+console.log(osszeAllit(ADATLISTA));
+function osszeAllit(lista) {
+  let txt = "";
+
+  txt += `<table class="table table-stripped ">`;
+  txt += `<tr class="table-dark"><th>név</th>
+    <th>kor</th>
+    <th>fajta</th></tr>`;
+  for (let index = 0; index < lista.length; index++) {
+    txt += `<tr>`;
+    for (const key in lista[index]) {
+      txt += `<td>${lista[index][key]}</td>`;
+    }
+    `</tr>`;
+  }
+  txt += `</table>`;
+  console.log(txt);
+  return txt;
 }
