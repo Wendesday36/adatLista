cb08e89242f3b3017b624fda9272e546ccb3e7a8
Wendesday36 <bertazoeanna@gmail.com>
2023-03-31 13:34:55 +0200

    pretier

--- JS/szuresek.js
+++ JS/szuresek.js
@@ -1,26 +1,24 @@
-
-
 export function szures(lista, kulcs, szuresiFeltetel) {
-    const SZURTLISTA = lista.filter(function (a) {
-      return a[kulcs].includes(szuresiFeltetel);
-    });
-    return SZURTLISTA;
-  }
-  const NEVINPUTELEM = $("knev");
-  const KORINPUTELEM = $("kkor");
-  const FAJTAINPUTELEM = $("kfajta");
-  NEVINPUTELEM.on("keyup", function () {
-    let nevErtek = NEVINPUTELEM.val();
-    let szurtList = szures(ADATLISTA, "nev", nevErtek);
-    console.log(szurtList);
+  const SZURTLISTA = lista.filter(function (a) {
+    return a[kulcs].includes(szuresiFeltetel);
   });
-  KORINPUTELEM.on("keyup", function () {
-    let korErtek = KORINPUTELEM.val();
-    let szurtList = szures(ADATLISTA, "kor", korErtek);
-    console.log(szurtList);
-  });
-  FAJTAINPUTELEM.on("keyup", function () {
-    let fajtaErtek = FAJTAINPUTELEM.val();
-    let szurtList = szures(ADATLISTA, "fajta", fajtaErtek);
-    console.log(szurtList);
-  });
+  return SZURTLISTA;
+}
+const NEVINPUTELEM = $("knev");
+const KORINPUTELEM = $("kkor");
+const FAJTAINPUTELEM = $("kfajta");
+NEVINPUTELEM.on("keyup", function () {
+  let nevErtek = NEVINPUTELEM.val();
+  let szurtList = szures(ADATLISTA, "nev", nevErtek);
+  console.log(szurtList);
+});
+KORINPUTELEM.on("keyup", function () {
+  let korErtek = KORINPUTELEM.val();
+  let szurtList = szures(ADATLISTA, "kor", korErtek);
+  console.log(szurtList);
+});
+FAJTAINPUTELEM.on("keyup", function () {
+  let fajtaErtek = FAJTAINPUTELEM.val();
+  let szurtList = szures(ADATLISTA, "fajta", fajtaErtek);
+  console.log(szurtList);
+});
